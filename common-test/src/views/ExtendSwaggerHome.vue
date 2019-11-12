d453c146b3a3f1b88e4e837aa9dc64c1052a80fc
chenqian <992091149@qq.com>
2019-11-12 13:45:35 +0800

    创建swagger文档扩展

---
+++ common-test/src/views/ExtendSwaggerHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-container>
+    <el-aside style="background: #2c3e50"></el-aside>
+    <el-main style="background: #42b983">
+      <el-header style="background: aquamarine"></el-header>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: 'ExtendSwaggerHome'
+  }
+</script>
+
+<style scoped>
+
+</style>
